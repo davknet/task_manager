4b468890381c7e986d0be4c962d72dda8d888b3b
D.Kahadze <davknet1@gmail.com>
2025-11-07 13:37:59 +0200

    feat:add migration of tasks table  feat: add migration of task_answers

---
+++ database/migrations/2025_11_07_112751_create_task_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('task_status');
+            $table->unsignedBigInteger('task_priority');
+            $table->string('title', 255);
+            $table->string('description', 900)->default('');
+            $table->timestamps();
+            $table->foreign('task_status')
+                  ->references('id')
+                  ->on('task_status')
+                  ->onDelete('cascade');
+
+            $table->foreign('task_priority')
+                  ->references('id')
+                  ->on('task_priority')
+                  ->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
