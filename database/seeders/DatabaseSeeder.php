d652ebea203830df33b5ed221f058daf67e78f47
D.Kahadze <davknet1@gmail.com>
2025-11-07 20:54:18 +0200

    feat: create user test user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,18 +18,19 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name'     => 'Test User',
+            'email'    => 'test@example.com',
+            'password' => Hash::make('1234')
+        ]);
 
         $this->call([
 
                TaskStatusSeeder::class ,
                TaskPrioritySeeder::class ,
                TaskSeeder::class,
-                 TaskAnswersSeeder::class ,
-                 IsAnswerCorrectSeeder::class ,
+               TaskAnswersSeeder::class ,
+               IsAnswerCorrectSeeder::class ,
 
         ]);
 
