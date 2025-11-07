757e34b72bb3ba6b726f39035f5b5b746cb6f5a6
D.Kahadze <davknet1@gmail.com>
2025-11-07 15:25:51 +0200

    docs: uncomment some classes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,8 +24,10 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
 
-            // TaskStatusSeeder::class ,
+               TaskStatusSeeder::class ,
                TaskPrioritySeeder::class ,
+               TaskSeeder::class,
+               
 
 
         ]);
