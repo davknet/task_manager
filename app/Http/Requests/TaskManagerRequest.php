f27f8fe781b860222d07a50c36b6450244bf5bb1
D.Kahadze <davknet1@gmail.com>
2025-11-07 20:53:28 +0200

    feat : add functions to TaskmanagerRequest

--- app/Http/Requests/TaskManagerRequest.php
+++ app/Http/Requests/TaskManagerRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\DB;
 
 class TaskManagerRequest extends FormRequest
 {
@@ -11,7 +13,7 @@ class TaskManagerRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true ;
     }
 
     /**
@@ -22,7 +24,60 @@ class TaskManagerRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'task_id'     => 'required|exists:tasks,id' ,
+            'priority_id' => 'required|exists:task_priority,id',
+            'status_id'   => 'required|exists:task_status,id' ,
+            'answer_id'   => 'required|exists:task_answers,id' ,
+            'user_id'     => 'required|exists:users,id'
         ];
     }
+
+
+    protected function withValidator(Validator $validator ){
+
+
+        $validator->after(function ($validator ) {
+
+             $this->makeDbValidation($validator);
+
+
+        });
+
+
+    }
+
+    protected function makeDbValidation(Validator $validator){
+
+
+           $priority_id = $this->input('priority_id') ;
+           $task_id     = $this->input('task_id');
+           $answer_id   = $this->input('task_id');
+           $status_id   = $this->input('status_id');
+           $user_id     = $this->input('user_id') ;
+
+
+
+           if( $priority_id > 1 ){
+
+               $low_priority = $priority_id - 1 ;
+
+                $valid = DB::table('task_manager')
+                ->where( 'priority_id' , $low_priority )
+                ->where('user_id' , $user_id)
+                ->where('task_id' , $task_id )
+                ->exists();
+
+                if(!$valid ){
+
+                    $validator->errors()->add('priority_id' , 'you mast to the lower priority task at first');
+                }
+
+           }
+
+
+
+
+    }
+
+
 }
